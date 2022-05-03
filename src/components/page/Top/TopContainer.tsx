7f37c116678ed19dd1fdfe2e8b07ad02f62a35ff
kou <k.ogawa.biz406@gmail.com>
2022-05-04 00:22:57 +0900

    ✨ 未ログインユーザーはログイン画面へ遷移させるように設定

--- src/components/page/Top/TopContainer.tsx
+++ src/components/page/Top/TopContainer.tsx
@@ -1,7 +1,18 @@
+import { useRouter } from 'next/router'
 import type { FC } from 'react'
+
+import { useAuth } from '@/hooks/auth'
 
 import { TopPresenter } from './TopPresenter'
 
 export const TopContainer: FC = () => {
+  const router = useRouter()
+  const { isLoading: isUserLoading, isSignedIn } = useAuth()
+
+  // 未ログインユーザーはログイン画面へ遷移
+  if (!isUserLoading && !isSignedIn) {
+    router.push('/auth/signin')
+  }
+
   return <TopPresenter />
 }
