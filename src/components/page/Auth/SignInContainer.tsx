a77c3e3b3211bd2bec81e9719362f2fe927e1151
kou <k.ogawa.biz406@gmail.com>
2022-05-05 00:02:14 +0900

    🚧 ログイン済みユーザーはログイン画面へ遷移できないように修正

--- src/components/page/Auth/SignInContainer.tsx
+++ src/components/page/Auth/SignInContainer.tsx
@@ -1,7 +1,17 @@
+import { useRouter } from 'next/router'
 import type { FC } from 'react'
+
+import { useAuth } from '@/hooks/auth'
 
 import { SignInPresenter } from './SignInPresenter'
 
 export const SignInContainer: FC = () => {
+  const router = useRouter()
+  const { isLoading, isSignedIn } = useAuth()
+
+  if (!isLoading && isSignedIn) {
+    router.push('/')
+  }
+
   return <SignInPresenter />
 }
