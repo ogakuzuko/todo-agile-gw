e113888b33c024215d2170858c03e223ce3b0a75
kou <k.ogawa.biz406@gmail.com>
2022-05-06 02:45:02 +0900

    💄 Loaderコンポーネント修正

--- src/components/ui/Loader/Loader.tsx
+++ src/components/ui/Loader/Loader.tsx
@@ -1,12 +1,19 @@
 import type { LoaderProps } from '@mantine/core'
-import { Grid, Loader as MantineLoader } from '@mantine/core'
+import { Loader as MantineLoader } from '@mantine/core'
 import type { FC } from 'react'
 
 export const Loader: FC<LoaderProps> = (props) => {
   return (
-    // NOTE: 何故かLoaderを h-screen(=100vh) にすると、ガクガクなって安定しなかったので、一時的に99vhにしている
-    <Grid justify="center" align="center" className="h-[99vh]">
-      <MantineLoader size="lg" color="pink" variant="bars" {...props} />
-    </Grid>
+    // ローディング背景用div
+    <div className="h-screen bg-orange-200/30">
+      <MantineLoader
+        size="lg"
+        color="pink"
+        variant="bars"
+        {...props}
+        // ローディングアイコンの中央寄せ
+        className="fixed inset-0 z-50 m-auto"
+      />
+    </div>
   )
 }
