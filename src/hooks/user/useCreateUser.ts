dbd0dfee66a81e8004413511dd068c8a48c6d012
kou <k.ogawa.biz406@gmail.com>
2022-05-06 11:41:02 +0900

    ♻️ useCreateUserリファクタリング

--- src/hooks/user/useCreateUser.ts
+++ src/hooks/user/useCreateUser.ts
@@ -1,36 +1,39 @@
 import { onAuthStateChanged } from 'firebase/auth'
-import { doc, getDoc } from 'firebase/firestore'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { createUserUsecase } from '@/domain/usecase/user'
-import { auth, firestore } from '@/libs/firebase'
+import { auth } from '@/libs/firebase'
+import { FirebaseUserClient } from '@/repositories/FirebaseUserClient'
 
 // Firestoreへのユーザー登録を行なうカスタムフック
 export const useCreateUser = (): void => {
   const [isUserCreated, setIsUserCreated] = useState(false)
 
-  // ユーザー登録されている場合は何もしない
-  if (isUserCreated) return
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      // 既にユーザー登録されている場合は何もしない
+      if (isUserCreated) return
 
-  onAuthStateChanged(auth, async (user) => {
-    if (user) {
-      const userDocRef = doc(firestore, 'users', user.uid)
-      const userSnapshot = await getDoc(userDocRef)
+      if (user) {
+        const firestoreUserData = await FirebaseUserClient.get(user.uid)
 
-      if (userSnapshot.exists()) {
-        setIsUserCreated(true)
-      }
-
-      // ユーザーが存在しない場合はFirestoreにユーザーデータを作成する
-      if (!userSnapshot.exists()) {
-        try {
-          const userData = { id: user.uid, name: user.displayName ?? undefined }
-          await createUserUsecase(userData)
+        if (firestoreUserData) {
           setIsUserCreated(true)
-        } catch (err) {
-          console.error('firestoreへのユーザー登録に失敗しました', err)
+        }
+
+        // ユーザーが存在しない場合はFirestoreにユーザーデータを作成する
+        if (!firestoreUserData) {
+          try {
+            const userData = { id: user.uid, name: user.displayName ?? undefined }
+            await createUserUsecase(userData)
+            setIsUserCreated(true)
+          } catch (err) {
+            console.error('firestoreへのユーザー登録に失敗しました', err)
+          }
         }
       }
-    }
-  })
+    })
+
+    return () => unsubscribe()
+  }, [isUserCreated])
 }
