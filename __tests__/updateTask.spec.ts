315774becbdcf8a1891edb2ace3f5de82db65f1e
kou <k.ogawa.biz406@gmail.com>
2022-05-08 19:05:01 +0900

    ✅ タスク更新ドメインロジックの正常系テストケースの作成

--- __tests__/updateTask.spec.ts
+++ __tests__/updateTask.spec.ts
@@ -2,9 +2,69 @@ import { VALID_DATA } from '@/constants/TEST_DATA/updateTask'
 import { updateTask } from '@/domain/entity/Task'
 
 describe('正規データ', () => {
-  // TODO: ちゃんと各フィールドが更新できるか一つずつテストケースを書く
-  it('課題のステータスを更新（開始前 → 開始前）', () => {
-    const result = updateTask(...VALID_DATA[0])
-    expect(result.status).toBe('BEFORE_START')
+  it('status更新（開始前 → 開始前）', () => {
+    // statusの更新をテストする系は、status部分のみのチェックに留める
+    const result = updateTask(...VALID_DATA[0].params)
+    expect(result.status).toBe(VALID_DATA[0].expected)
+  })
+  it('status更新（開始前 → 開始）', () => {
+    const result = updateTask(...VALID_DATA[1].params)
+    expect(result.status).toBe(VALID_DATA[1].expected)
+  })
+  it('status更新（開始 → 開始）', () => {
+    const result = updateTask(...VALID_DATA[2].params)
+    expect(result.status).toEqual(VALID_DATA[2].expected)
+  })
+  it('status更新（開始 → 開発終了）', () => {
+    const result = updateTask(...VALID_DATA[3].params)
+    expect(result.status).toEqual(VALID_DATA[3].expected)
+  })
+  it('status更新（開発終了 → 開発終了）', () => {
+    const result = updateTask(...VALID_DATA[4].params)
+    expect(result.status).toEqual(VALID_DATA[4].expected)
+  })
+  it('status更新（開発終了 → 検証環境デプロイ）', () => {
+    const result = updateTask(...VALID_DATA[5].params)
+    expect(result.status).toEqual(VALID_DATA[5].expected)
+  })
+  it('status更新（開発終了 → 開始）', () => {
+    const result = updateTask(...VALID_DATA[6].params)
+    expect(result.status).toEqual(VALID_DATA[6].expected)
+  })
+  it('status更新（検証環境デプロイ → 検証環境デプロイ）', () => {
+    const result = updateTask(...VALID_DATA[7].params)
+    expect(result.status).toEqual(VALID_DATA[7].expected)
+  })
+  it('status更新（検証環境デプロイ → テストOK）', () => {
+    const result = updateTask(...VALID_DATA[8].params)
+    expect(result.status).toEqual(VALID_DATA[8].expected)
+  })
+  it('status更新（検証環境デプロイ → テストNG）', () => {
+    const result = updateTask(...VALID_DATA[9].params)
+    expect(result.status).toEqual(VALID_DATA[9].expected)
+  })
+  it('status更新（テストOK → テストOK）', () => {
+    const result = updateTask(...VALID_DATA[10].params)
+    expect(result.status).toEqual(VALID_DATA[10].expected)
+  })
+  it('status更新（テストOK → リリース済み）', () => {
+    const result = updateTask(...VALID_DATA[11].params)
+    expect(result.status).toEqual(VALID_DATA[11].expected)
+  })
+  it('status更新（テストNG → テストNG）', () => {
+    const result = updateTask(...VALID_DATA[12].params)
+    expect(result.status).toEqual(VALID_DATA[12].expected)
+  })
+  it('status更新（テストNG → 開始）', () => {
+    const result = updateTask(...VALID_DATA[13].params)
+    expect(result.status).toEqual(VALID_DATA[13].expected)
+  })
+  it('status更新（テストNG → 検証環境デプロイ）', () => {
+    const result = updateTask(...VALID_DATA[14].params)
+    expect(result.status).toEqual(VALID_DATA[14].expected)
+  })
+  it('status更新（リリース済み → リリース済み）', () => {
+    const result = updateTask(...VALID_DATA[15].params)
+    expect(result.status).toEqual(VALID_DATA[15].expected)
   })
 })
--- src/constants/TEST_DATA/updateTask.ts
+++ src/constants/TEST_DATA/updateTask.ts
@@ -1,25 +1,257 @@
 // updateTask()のテスト用データ
 import type { updateTask } from '@/domain/entity/Task'
-// import type { Task } from '@/types/task'
+import type { Status } from '@/types/task'
 
-type UpdateTaskDataParams = Parameters<typeof updateTask>
+type ValidStatusTestData = {
+  params: Parameters<typeof updateTask>
+  expected: Status // status部分が期待通りであるか
+}
 
-// type InvalidTestData = {
-//   params: Parameters<typeof updateTask>
-//   expected: string // エラーメッセージ
-// }
+type InvalidTestData = {
+  params: Parameters<typeof updateTask>
+  expected: string // エラーメッセージ
+}
 
 // タスクの更新として有効なデータ
-export const VALID_DATA: UpdateTaskDataParams[] = [
-  // 変更前：開始前 → 変更後：開始前
-  [
-    {
-      id: 'TEST_TASK',
-      title: 'テストタスク',
-      status: 'BEFORE_START',
-      userId: 'TEST_USER',
-      type: 'FEATURE',
-    },
-    'BEFORE_START',
-  ],
+export const VALID_DATA: ValidStatusTestData[] = [
+  // 開始前 → 開始前（変更なし）
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'BEFORE_START',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'BEFORE_START',
+    ],
+    expected: 'BEFORE_START',
+  },
+  // 開始前 → 開始
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'STARTED',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'BEFORE_START',
+    ],
+    expected: 'STARTED',
+  },
+  // 開始 → 開始（変更なし）
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'STARTED',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'STARTED',
+    ],
+    expected: 'STARTED',
+  },
+  // 開始 → 開発終了
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'DEV_FINISHED',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'STARTED',
+    ],
+    expected: 'DEV_FINISHED',
+  },
+  // 開発終了 → 開発終了（変更なし）
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'DEV_FINISHED',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'DEV_FINISHED',
+    ],
+    expected: 'DEV_FINISHED',
+  },
+  // 開発終了 → 検証環境デプロイ
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'VER_DEPLOYED',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'DEV_FINISHED',
+    ],
+    expected: 'VER_DEPLOYED',
+  },
+  // 開発終了 → 開始
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'STARTED',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'DEV_FINISHED',
+    ],
+    expected: 'STARTED',
+  },
+  // 検証環境デプロイ → 検証環境デプロイ（変更なし）
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'VER_DEPLOYED',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'VER_DEPLOYED',
+    ],
+    expected: 'VER_DEPLOYED',
+  },
+  // 検証環境デプロイ → テストOK
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'TEST_OK',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'VER_DEPLOYED',
+    ],
+    expected: 'TEST_OK',
+  },
+  // 検証環境デプロイ → テストNG
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'TEST_NG',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'VER_DEPLOYED',
+    ],
+    expected: 'TEST_NG',
+  },
+  // テストOK → テストOK（変更なし）
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'TEST_OK',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'TEST_OK',
+    ],
+    expected: 'TEST_OK',
+  },
+  // テストOK → リリース済み
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'RELEASED',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'TEST_OK',
+    ],
+    expected: 'RELEASED',
+  },
+  // テストNG → テストNG（変更なし）
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'TEST_NG',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'TEST_NG',
+    ],
+    expected: 'TEST_NG',
+  },
+  // テストNG → 開始
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'STARTED',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'TEST_NG',
+    ],
+    expected: 'STARTED',
+  },
+  // テストNG → 検証環境デプロイ
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'VER_DEPLOYED',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'TEST_NG',
+    ],
+    expected: 'VER_DEPLOYED',
+  },
+  // リリース済み → リリース済み
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'RELEASED',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'RELEASED',
+    ],
+    expected: 'RELEASED',
+  },
+]
+
+export const INVALID_DATA: InvalidTestData[] = [
+  {
+    params: [
+      {
+        id: 'TEST_TASK',
+        title: 'テストタスク',
+        status: 'BEFORE_START',
+        userId: 'TEST_USER',
+        type: 'FEATURE',
+      },
+      'BEFORE_START',
+    ],
+    expected: 'エラーメッセージ',
+  },
 ]
